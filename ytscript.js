dcf84be566dcbef2c9a265ff766892e96099406a
Jacob <jacob.erickson@mail.utoronto.ca>
2017-01-10 21:12:13 -0500

    added function to ui

--- ytscript.js
+++ ytscript.js
@@ -1,6 +1,6 @@
-
-  var player;
-  function onYouTubeIframeAPIReady(pID) {
+//some code referenced from jsfiddle/MadLittleMods and tutorialzine
+var player;
+function onYouTubeIframeAPIReady(pID) {
 
     var ctrlq = document.getElementById("youtube-audio");
     ctrlq.innerHTML = '<img id="youtube-icon" src="img/ic_pause_black_48dp.png"/><div id="youtube-player"></div>';
@@ -24,33 +24,60 @@
         'onStateChange': onPlayerStateChange
       }
     });
-  }
+}
 
-  function togglePlayButton(play) {
+function togglePlayButton(play) {
     document.getElementById("youtube-icon").src = play ? "img/ic_pause_black_48dp.png" : "img/ic_play_arrow_black_48dp.png";
-  }
+}
 
-  function toggleAudio() {
+function toggleAudio() {
     if ( player.getPlayerState() == 1 || player.getPlayerState() == 3 ) {
-      player.pauseVideo();
-      togglePlayButton(false);
+        player.pauseVideo();
+        togglePlayButton(false);
     } else {
-      player.playVideo();
-      togglePlayButton(true);
+        player.playVideo();
+        togglePlayButton(true);
     }
-  }
+}
 
-  function onPlayerReady(event) {
+function onPlayerReady(event) {
     player.setPlaybackQuality("large");
     document.getElementById("youtube-audio").style.display = "block";
     togglePlayButton(player.getPlayerState() !== 5);
-  }
 
-  function onPlayerStateChange(event) {
+    //adding functionality to ui
+
+    updateProgressBar();
+    time_update_interval = setInterval(function(){
+        updateProgressBar();
+    }, 1000)
+
+    $("#progress-bar").on("mouseup touchend", function(e) {
+        var newTime = player.getDuration() * (e.target.value / 100);
+        player.seekTo(newTime);
+    });
+
+    $("#skipf").on("click", function(){
+        player.nextVideo();
+        $("#youtube-icon").attr("src", "img/ic_pause_black_48dp.png");
+    });
+
+    $("#skipb").on("click", function(){
+        player.previousVideo();
+        $("#youtube-icon").attr("src", "img/ic_pause_black_48dp.png");
+    });
+
+}
+
+function onPlayerStateChange(event) {
     if (event.data === 0) {
       togglePlayButton(false);
     }
     if (player.getPlayerState() == 1) {
         document.getElementById("subtext").innerText = player.getVideoData().title;
     }
-  }
+}
+
+function updateProgressBar(){
+    $("#progress-bar").val((player.getCurrentTime() / player.getDuration()) * 100);
+}
